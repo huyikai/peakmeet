8351697c28062f8bed5e64b3a2b3dc3af69ce582
huyikai <hyk_tiger@foxmail.com>
2026-07-24 15:48:25 +0800

    style(miniprogram): normalize app.js indentation

--- packages/miniprogram/app.js
+++ packages/miniprogram/app.js
@@ -1,13 +1,13 @@
 "use strict";
 App({
-  onLaunch() {
-    if (!wx.cloud) {
-      console.error('请使用支持云开发的基础库');
-      return;
-    }
-    wx.cloud.init({
-      env: 'cloud1-d8ghafmni1c847e3f',
-      traceUser: true,
-    });
-  },
+    onLaunch() {
+        if (!wx.cloud) {
+            console.error('请使用支持云开发的基础库');
+            return;
+        }
+        wx.cloud.init({
+            env: 'cloud1-d8ghafmni1c847e3f',
+            traceUser: true,
+        });
+    },
 });
